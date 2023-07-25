b58aeef6ae054b222115855cfeb7927ff0d8a25a
Parker Moesta <75709283+DimensionDweller@users.noreply.github.com>
2023-07-24 20:02:44 -0400

    Create embedding_utils.py

---
+++ embedding_utils.py
@@ -0,0 +1,18 @@
+def get_embeddings(terms, tokenizer, text_encoder):
+    """
+    Get embeddings for a list of terms.
+    
+    Args:
+    - terms (list of str): Terms to get embeddings for.
+    - tokenizer: Tokenizer for the text model.
+    - text_encoder: Text encoder model.
+    
+    Returns:
+    - dict: Dictionary of terms and their embeddings.
+    """
+    embeddings = {}
+    for term in terms:
+        input_text = tokenizer(term, return_tensors="pt").input_ids.to(torch_device)
+        with torch.no_grad():
+            embeddings[term] = text_encoder(input_text).pooler_output.squeeze().cpu().numpy()
+    return embeddings
